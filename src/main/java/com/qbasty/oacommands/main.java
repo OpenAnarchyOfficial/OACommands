00d84cb28fbe61cf5ad297d3e4a762e2d4432865
qbasty <58854397+qbasty@users.noreply.github.com>
2021-02-07 16:34:24 +0100

    fixed config

--- src/main/java/com/qbasty/oacommands/main.java
+++ src/main/java/com/qbasty/oacommands/main.java
@@ -1,12 +1,25 @@
 package com.qbasty.oacommands;
 
+import com.google.gson.Gson;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
 
 public final class main extends JavaPlugin {
 
+    public static File dataFolder;
+    public static Gson gson = new Gson();
+    public static main INSTANCE;
+
     @Override
     public void onEnable() {
+
+        INSTANCE = this;
+        saveDefaultConfig();
+        dataFolder = this.getDataFolder();
+
         // Plugin startup logic
         this.getCommand("discord").setExecutor((CommandExecutor)new text(this));
         this.getCommand("donate").setExecutor((CommandExecutor)new text(this));
